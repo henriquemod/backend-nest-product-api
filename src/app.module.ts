26d6c82a3ac257a1539968f7b5618e15dd3f210c
Henrique Souza <henrique.asouza@outlook.com>
2024-06-08 00:35:45 -0300

    chore: add docker build project

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 
+const host = process.env.DB_HOST || '127.0.0.1';
 const db = process.env.DB_NAME || 'product-api';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      `mongodb://127.0.0.1:27017/${db}?retryWrites=true&loadBalanced=false&replicaSet=rs0&readPreference=primary&connectTimeoutMS=10000`,
-    ),
+    MongooseModule.forRoot(`mongodb://${host}:27017/${db}`),
     ProductModule,
   ],
 })
